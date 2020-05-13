a113f954c00569673725684e2088e699ab67ac45
Piotr Żegliński <piotr.zeglinski@gmail.com>
2020-05-13 20:40:24 +0100

    homework IV week

--- main.js
+++ main.js
@@ -2,5 +2,13 @@ const firstName = "Piotr";
 const age = 37;
 
 console.log(
-  `Cześć, miło Cię widzieć, nazywam się ${firstName} i mam ${age} lat.`
+  `Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`
 );
+
+const heading = document.querySelector('.main__heading--js');
+
+heading.innerHTML = `Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`;
+
+const emptyParagraph = document.querySelector('.why__description--js');
+
+emptyParagraph.innerHTML = `Chętnie rozpocznę staż albo przyjmę zlecenie na prostą stronkę ;)`;
