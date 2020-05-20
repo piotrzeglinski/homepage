d5bc4788c0ceaab43739fbaf1d660c24d3681dd5
Piotr Żegliński <piotr.zeglinski@gmail.com>
2020-05-20 18:47:55 +0100

    BEM & background final changes

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const firstName = "Piotr";
 const age = 37;
 
-console.log(
-  `Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`
-);
-
 const heading = document.querySelector('.main__heading--js');
 
 heading.innerHTML = `Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`;
 
-const emptyParagraph = document.querySelector('.why__description--js');
+const greet = (age, firstName) => {
+  console.log(`Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`);
+}
 
-emptyParagraph.innerHTML = `Chętnie rozpocznę staż albo przyjmę zlecenie na prostą stronkę ;)`;
+greet(age, firstName);
+greet('18', 'Franio');
+
+function createContent(querySelectorContent, content) {
+const element = document.querySelector(querySelectorContent);
+element.innerHTML = content;
+}
+
+createContent('.why__description--js', 'Chętnie nawiążę współpracę. Odezwij się jeśli szukasz kogoś do pomocy!');
