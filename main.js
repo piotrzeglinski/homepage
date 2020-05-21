f10278c28e124eb5b4c0a738720e50fc242d42da
Piotr Żegliński <piotr.zeglinski@gmail.com>
2020-05-21 13:12:29 +0100

    JS greet complete

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 const firstName = "Piotr";
 const age = 37;
 
-const heading = document.querySelector('.main__heading--js');
-
-heading.innerHTML = `Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`;
-
 const greet = (age, firstName) => {
   console.log(`Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`);
 }
 
-greet(age, firstName);
-greet('18', 'Franio');
+greet(37, 'Piotr');
 
 function createContent(querySelectorContent, content) {
 const element = document.querySelector(querySelectorContent);
 element.innerHTML = content;
 }
 
-createContent('.why__description--js', 'Chętnie nawiążę współpracę. Odezwij się jeśli szukasz kogoś do pomocy!');
+createContent('.why__description--js', 'Chętnie nawiążę współpracę. Odezwij się jeśli szukasz kogoś do pomocy! ;)');
