90bc0f2893728ec99752b6ee8686d4e9ef241c8e
Piotr Żegliński <piotr.zeglinski@gmail.com>
2020-06-06 13:32:28 +0100

    gitignore package.json scss

--- main.js
+++ main.js
@@ -1,15 +1,26 @@
 const firstName = "Piotr";
 const age = 37;
 
-const greet = (age, firstName) => {
-  console.log(`Siema, fajnie, że wpadłeś, nazywam się ${firstName} i mam ${age} lat.`);
-}
-
-greet(37, 'Piotr');
-
 function createContent(querySelectorContent, content) {
 const element = document.querySelector(querySelectorContent);
 element.innerHTML = content;
 }
 
 createContent('.why__description--js', 'Chętnie nawiążę współpracę. Odezwij się jeśli szukasz kogoś do pomocy! ;)');
+
+const button = document.querySelector('.action--js');
+
+console.log(button)
+
+button.addEventListener('click', () => {
+  const heading = document.querySelector(".main__heading--js");
+  heading.innerHTML = `CZEŚĆ, UCZĘ SIĘ BUDOWAĆ STRONĘ INTERNETOWĄ`;
+  heading.classList.toggle('nowa-klasa-z-js');
+});
+
+const hamburger = document.querySelector('.hamburger--js');
+
+hamburger.addEventListener('click', () => {
+  const nav = document.querySelector('.navigation--js');
+  nav.classList.toggle('navigation__open');
+});
